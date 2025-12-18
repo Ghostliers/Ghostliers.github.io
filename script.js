c91d2ba08623308d293c564ffad328dc19e92a1a
Ghostliers <diegoalonso.casazola06@gmail.com>
2025-12-18 16:51:09 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function startGame(){
+    document.getElementById("menu-container").style.display= "none";
+    document.getElementById("game-container").style.display= "flex";
+}
+document.getElementById("play-button").addEventListener("click", startGame);
+document.getElementById("game-container").style.display="none";
+
+
+    
